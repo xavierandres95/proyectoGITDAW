ff7c1a07891de7316ad2a5977d41312497f20904
'Xavier <xavierandres.tepud-yaguari@iessampedro.org>
2022-03-16 20:57:41 +0100

    Subida Github

--- ProyectoGIT/src/Ejercicio1.java
+++ ProyectoGIT/src/Ejercicio1.java
@@ -7,7 +7,7 @@ public class Ejercicio1 {
 		
 		System.out.println("Hola");
 		System.out.println("Vuelvo a saludar");
-		System.out.println("Subida conectadaa a GIThub");
+		System.out.println("Subida conectadaa a GiThub");
 		
 	}
 
